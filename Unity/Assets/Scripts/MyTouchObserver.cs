3ad1acd7b0f782ef6a3118b92d3ab78ff1bf5e37
Tim Sun <tsun@sagosago.com>
2020-11-10 15:00:21 -0500

    added and registered touch observer

---
+++ Unity/Assets/Scripts/MyTouchObserver.cs
@@ -0,0 +1,37 @@
+﻿namespace TouchExample {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using SagoTouch;
+    using Touch = SagoTouch.Touch;
+
+    public class MyTouchObserver : MonoBehaviour, ISingleTouchObserver {
+
+        public bool OnTouchBegan(Touch touch) {
+            Debug.Log("touched");
+            return false;
+        }
+
+        public void OnTouchCancelled(Touch touch) {
+        }
+
+        public void OnTouchEnded(Touch touch) {
+        }
+
+        public void OnTouchMoved(Touch touch) {
+        }
+
+        void OnEnable() {
+            if (TouchDispatcher.Instance) {
+                TouchDispatcher.Instance.Add(this, 0, true);
+            }
+        }
+
+        void OnDisable() {
+            if (TouchDispatcher.Instance) {
+                TouchDispatcher.Instance.Remove(this);
+            }
+        }
+
+    }
+}
