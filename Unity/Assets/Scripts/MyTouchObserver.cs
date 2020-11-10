5377aec40b5774da93d4a15597665b4b6268d9c8
Tim Sun <tsun@sagosago.com>
2020-11-10 15:35:41 -0500

    hit detection

--- Unity/Assets/Scripts/MyTouchObserver.cs
+++ Unity/Assets/Scripts/MyTouchObserver.cs
@@ -7,15 +7,46 @@
 
     public class MyTouchObserver : MonoBehaviour, ISingleTouchObserver {
 
-        public bool OnTouchBegan(Touch touch) {
+		#region Properties
+
+		[System.NonSerialized]
+        private Camera m_Camera;
+
+        [System.NonSerialized]
+        private Renderer m_Renderer;
+
+        [System.NonSerialized]
+        private Transform m_Transform;
+
+        public Camera Camera {
+            get { return m_Camera = m_Camera ?? CameraUtils.FindRootCamera(this.Transform); }
+        }
+
+        public Renderer Renderer {
+            get { return m_Renderer = m_Renderer ?? GetComponent<Renderer>(); }
+        }
+
+        public Transform Transform {
+            get { return m_Transform = m_Transform ?? GetComponent<Transform>(); }
+        }
+
+		#endregion
+
+
+		#region Touch
+
+		public bool OnTouchBegan(Touch touch) {
             Debug.Log("touched");
-            return false;
+            Bounds bounds = this.Renderer.bounds;
+            bounds.extents += Vector3.forward;
+            return bounds.Contains(CameraUtils.TouchToWorldPoint(touch, Transform, Camera));
         }
 
         public void OnTouchCancelled(Touch touch) {
         }
 
         public void OnTouchEnded(Touch touch) {
+            Debug.Log("touch ended");
         }
 
         public void OnTouchMoved(Touch touch) {
@@ -33,5 +64,7 @@
             }
         }
 
-    }
+		#endregion
+
+	}
 }
