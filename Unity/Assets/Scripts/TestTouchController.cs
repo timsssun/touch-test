d632e985df85ec3d76d431aa2d407162b2145e4f
Tim Sun <tsun@sagosago.com>
2020-11-10 18:26:15 -0500

    added buttons to disable and enable all touch

---
+++ Unity/Assets/Scripts/TestTouchController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SagoTouch;
+
+public class TestTouchController : MonoBehaviour
+{
+	#region Methods
+
+	public void DisableAllTouch() {
+		if (TouchDispatcher.Instance) {
+			TouchDispatcher.Instance.enabled = false;
+		}
+	}
+
+	public void EnableAllTouch() {
+		if (TouchDispatcher.Instance) {
+			TouchDispatcher.Instance.enabled = true;
+		}
+	}
+
+	#endregion
+}
