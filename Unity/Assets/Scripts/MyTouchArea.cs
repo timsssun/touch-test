705073d62c927842c28d1ce2ac828eebb6b0d622
Tim Sun <tsun@sagosago.com>
2020-11-10 18:22:44 -0500

    buttons to enable and disable touch area

--- Unity/Assets/Scripts/MyTouchArea.cs
+++ Unity/Assets/Scripts/MyTouchArea.cs
@@ -1,18 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SagoTouch;
+using Touch = SagoTouch.Touch;
 
 public class MyTouchArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	#region Fields
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[System.NonSerialized]
+	private TouchArea m_TouchArea;
+
+	[System.NonSerialized]
+	private TouchAreaObserver m_TouchAreaObserver;
+
+	[System.NonSerialized]
+	private Transform m_Transform;
+
+	#endregion
+
+
+	#region Properties
+
+	public TouchArea TouchArea {
+		get { return m_TouchArea = m_TouchArea ?? GetComponent<TouchArea>(); }
+	}
+
+	public TouchAreaObserver TouchAreaObserver {
+		get { return m_TouchAreaObserver = m_TouchAreaObserver ?? GetComponent<TouchAreaObserver>(); }
+	}
+
+	public Transform Transform {
+		get { return m_Transform = m_Transform ?? GetComponent<Transform>(); }
+	}
+
+	#endregion
+
+
+	#region Methods
+
+	private void OnEnable() {
+		this.TouchAreaObserver.TouchUpDelegate = ReactTouchUp;
+		this.TouchAreaObserver.TouchDownDelegate = ReactTouchDown;
+	}
+
+	private void OnDisable() {
+		this.TouchAreaObserver.TouchUpDelegate = null;
+	}
+
+	private void ReactTouchUp(TouchArea touchArea, Touch touch) {
+		this.Transform.localScale = Vector3.one;
+	}
+
+	private void ReactTouchDown(TouchArea touchArea, Touch touch) {
+		this.Transform.localScale = Vector3.one * 0.8f;
+	}
+
+	public void DisableTouchArea() {
+		this.TouchArea.enabled = false;
+	}
+
+	public void EnableTouchArea() {
+		this.TouchArea.enabled = true;
+	}
+
+	#endregion
+
 }
